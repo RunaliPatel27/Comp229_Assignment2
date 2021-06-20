08d31a1e642e02c6a17fdd11d2edb16a17d7c5bf
Runali Patel <rpate629@my.centennialcollege.ca>
2021-06-20 16:38:54 -0400

    Assignment 2 completed!

---
+++ RunaliPatel-Assignment2/Comp229-Assignment2-Runali/README.md
@@ -0,0 +1,3 @@
+# Comp229-Assignment2
+Express Portfolio Authentication
+Using Portfolio Web Site, restructured the web site to include a secure section that displays a list of contents
---
+++ RunaliPatel-Assignment2/Comp229-Assignment2-Runali/server/config/db.js
@@ -0,0 +1,5 @@
+module.exports =
+{
+    //"URI": "mongodb://localhost/contact_list"
+    "URI":"mongodb+srv://runalipatel:WbOrIXvNUSFTLlC8@runali.3wxg2.mongodb.net/contact_list?retryWrites=true&w=majority"
+}
